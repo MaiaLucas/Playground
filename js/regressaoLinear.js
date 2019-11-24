dbd3dde482d5534fb740ff663dfdfcf9b232237a
MaiaLucas <barretomaia5466@outlook.com>
2019-11-24 20:22:33 -0300

    Criando a interface dos algoritmos - inicialmente bem basico

--- js/regressaoLinear.js
+++ js/regressaoLinear.js
@@ -1,3 +1,8 @@
+// HTML
+let p = document.createElement('p')
+// let app = document.getElementById('app')
+let app = $('#app')
+
 // FUNÇÃO QUE CRIA OS DADOS
 function Data(x, y) {
 	this.x = x // ano
@@ -79,37 +84,56 @@ function intercept(data) {
 }
 
 function printEq(data) {
-    console.log(`A equação da reta: pop = ${slope(data)} * ano + (${intercept(data)})`)
+	let div = $('<div></div>')
+	console.log(`A equação da reta: pop = ${slope(data)} * ano + (${intercept(data)})`)
+	div.append(`A equação da reta: pop = ${slope(data)} * ano + (${intercept(data)})`)
+	app.append(div)
 }
 
 // FUNÇÃO RESPONSÁVEL POR DESCOBRIR O VALOR DA POSIÇÃO Y DE UM NOVO DADO
 function discoverY(data,x) {
 	let pop = (slope(data) * x) + intercept(data)
-	
+	let div = $('<div></div>')
 	console.log(`Calculo da Populacao do ano ${x} é ${pop.toFixed(2)}`)
+	div.append(`Calculo da Populacao do ano ${x} é ${pop.toFixed(2)}`)
+	app.append(div)
+
 	data.push(new Data(x, pop))
 }
 
 // Imprime todas as informações
 function print(data) {
+	let div = $('<div></div>')
+	div.append(`---------------------------------------<br/>`)
 	for(let i = 0; i < data.length; i++) {
-		console.log(`Dado[${i + 1}] : X -> ${data[i].x}, Y -> ${data[i].y}, XY -> ${data[i].xy}`)
+		// console.log(`Dado[${i + 1}] : X -> ${data[i].x}, Y -> ${data[i].y}, XY -> ${data[i].xy}`)
+		div.append(`Dado[${i + 1}] : ANO -> ${data[i].x}, POP -> ${data[i].y}, (ANO x POP) -> ${data[i].xy} <br/>`)
 	}
+	div.append(`---------------------------------------<br/>`)
+	// console.log(`Somatorio Y = ${sumY(data)}`)
+	div.append(`
+	Somatorio POP = ${sumY(data)} <br />
+	Somatorio ANO = ${sumX(data)} <br />
+	Média ANO = ${midX(data)} <br />
+	Média POP = ${midY(data)} <br />
+	`)
+	// console.log(`Somatorio X = ${sumX(data)}`)
+	// console.log(`Média X = ${midX(data)}`)
+	// console.log(`Média Y = ${midY(data)}`)
 
-	console.log(`Somatorio X = ${sumX(data)}`)
-	console.log(`Somatorio Y = ${sumY(data)}`)
-	console.log(`Média X = ${midX(data)}`)
-	console.log(`Média Y = ${midY(data)}`)
+	app.append(div)
 }
 
 function start() {
-	console.log(dados)
+	// console.log(dados)
+	let input = $('#year').val()
+	app.append(`<br /><h1> O ano escolhido foi: ${input} </h1> <br />`)
 	print(dados)
 	printEq(dados)
-	discoverY(dados, 2005)
-	console.log('*----------------------------------------------------*')
+	discoverY(dados, input)
+	app.append(`<h3>Nova listagem</h3>`)
 	print(dados)
-	console.log(dados)
+	// console.log(dados)
 }
 
 let D1 = new Data(1980, 2.1)
@@ -120,4 +144,5 @@ let D5 = new Data(2000, 4.9)
 
 let dados = [D1, D2, D3, D4, D5]
 
-start()
+// start()
+// app.append(p)
